cec5f74bcdc1b01e7c5d081fd0e81f77ec661259
Farkhad <farkhad@wizardsdev.com>
2020-03-26 17:39:51 +0400

    version 1.1.0

---
+++ src/main/java/module1/subject/GuessTheNumber.java
@@ -0,0 +1,17 @@
+package module1.subject;
+
+import java.util.Scanner;
+
+//Задача 4. Угадать число, которое загадал пользователь, используя if-else.
+public class GuessTheNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Загадайте в уме чило от 1 до 10");
+
+        System.out.println("Ваше число больше 3-х? (y/n) ");
+        char c1;
+        c1 = (char) System.in.;
+        System.out.println("c1 = " +c1);
+
+    }
+}
